945bb8405e6d3472ac1e42d838288571efbbb18e
helberte costa arruda <helberteads@gmail.com>
2022-10-09 17:11:44 -0400

    Pegando filme aleatorio para mostrar na capa inicial

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import Tmdb from './Tmdb';
 import MovieRow from './components/MovieRows';
+import FeaturedMovie from './components/featuredMovie';
 
 
 export default () => {
 
   const [movieList, setMovieList] = useState([]);
-
+  const [featuredDate, setFeaturedData] = useState(null);
 
   useEffect(() => {
     const loadAll = async () => {
@@ -15,6 +16,13 @@ export default () => {
 
       let list = await Tmdb.getHomeList();
       setMovieList(list);
+
+      //pegando o featured filme em destaque
+      let originais = list.filter(i=>i.slug === 'originais');
+      let randomChosen = Math.floor(Math.random() * (originais[0].items.results.length -1));
+      let chosen = originais[0].items.results[randomChosen];
+
+      console.log(chosen);
     }
 
     loadAll();
@@ -25,6 +33,11 @@ useEffect significa que quando a tela for carregada ele vai executar a função 
 
   return(
     <div className='page'>
+
+      {featuredDate &&
+        <featuredDate item={featuredDate} />
+      }
+
       <section className='lists'>
         {movieList.map((item, key) => (
           <MovieRow key={key} title={item.title} items={item.items} />
